318aa97b1c37c9d2186306e500d75cfe02cebd58
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-03-08 15:27:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola test</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="main">
+
+<img src="./assets/logo2.png" alt="">
+
+    <div class="sidebar">
+    <h1>
+        Hola mamacita
+    </h1>
+	<h1>
+		Pedro miguel pauleta
+		
+	</h1>
+
+
+    </div>
+    <div class="Footer">
+
+    </div>
+   
+</body>
+</html>
