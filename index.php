5736bfe674967f4b152cdd1a3eb58c34eede6755
wassimouricha <wbouricha5@gmail.com>
2022-03-08 16:15:39 +0100

    latest update

--- index.php
+++ index.php
@@ -6,20 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hola test</title>
     <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+   
 </head>
 <body class="main">
 
 <img src="./assets/logo2.png" alt="">
 
     <div class="sidebar">
-    <h1>
-        bustronome
-    </h1>
-	<h1>
-		Pedro miguel pauleta
-		
-	</h1>
+        <div class="logo_container">
+       <div class="logo">
+       
+           <div class="logo_name">  <h1> <i class='bx bx-star'></i> Bustronome  <i class='bx bx-star'></i> </h1>  </div>
+           <i class='bx bx-menu' ></i>
+       </div>
 
+        </div>
 
     </div>
     <div class="Footer">
