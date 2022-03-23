44aee3a34040a0115bac69ae63a630c9ad2dedbb
wassimouricha <wbouricha5@gmail.com>
2022-03-23 10:50:03 +0100

    latest update php

---
+++ includes/formulaire.php
@@ -0,0 +1,32 @@
+<div class="containere">
+        
+           
+
+        <form class="container" method="POST"> <!-- ici j'ajoute la method HTTP post afin d'indiquer que je vais envoyer des informations -->
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Pseudo</label>
+                    <input type="text" class="form-control" name="pseudo">
+                    <div id="emailHelp" class="form-text">Je ne vais jamais partager vos informations vous pensez bien</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Nom</label>
+                    <input type="text" class="form-control" name="alstname">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Prénom</label>
+                    <input type="text" class="form-control" name="firstname">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
+                    <input type="password" class="form-control" name="password">
+                </div>
+        
+                <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
+        </form>
+    
+
+        </form>
+          
+   
+</div>
---
+++ page_de_connexion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- En tete -->
+<?php include 'includes/head.php' ; ?>
+
+<!-- Body -->
+<body class="main">
+
+
+            <!-- sidebar -->
+            <?php include 'includes/sidebar.php' ; ?>
+            <!-- Mot flottant -->
+            <?php include 'includes/motflottant.php' ; ?>
+            <!-- automatic carousel slider -->
+            <?php include 'includes/formulaire.php' ; ?>
+            <!-- Boutton dropped down  -->
+            <?php include 'includes/buttondropped.php' ; ?>
+            <!-- Bulle card text -->
+            <?php include 'includes/cardtext.php' ; ?>
+            <!-- footer -->
+            <?php include 'includes/footer.php' ; ?>
+
+    <!-- Script -->
+      <script src="./script.js"></script>
+</body>
+</html>
