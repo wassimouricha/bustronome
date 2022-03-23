b3af32174a3b8fb8c7caf164b59868545fa9d83e
wassimouricha <wbouricha5@gmail.com>
2022-03-23 13:49:06 +0100

    latest php update

--- actions/signupaction.php
+++ actions/signupaction.php
@@ -16,6 +16,11 @@ require ('actions/database.php') ;
 if(isset($_POST['validate'])){
 //  ici on va verifier si toutes les champs ne sont pas vides
     if(!empty($_POST['pseudo']) AND !empty($_POST['lastname']) AND !empty($_POST['firstname']) AND !empty($_POST['password']) ){
+            //ici on déclare nos varibales qui vont contenir nos champs 
+        $user_pseudo = htmlspecialchars($_POST['pseudo']);
+        $user_lastname = htmlspecialchars($_POST['lastname']);
+        $user_firstname = htmlspecialchars($_POST['firstname']);
+        $user_password = htmlspecialchars($_POST['password']);
 
     }else{
         $errorMsg = " Veuillez compléter tous les champs....";
