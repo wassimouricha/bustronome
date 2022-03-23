525e5d42d919af8b03ef4ea5a63570798b9558a8
wassimouricha <wbouricha5@gmail.com>
2022-03-23 15:30:45 +0100

    latest php update

---
+++ actions/securiteaction.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+//je vais faire une condition pour permettre de voir si l'utilisateur est authentifier 
+if(!isset($_SESSION['auth'])){
+    header('Location: login.php');
+}
---
+++ ajoutmenu.php
@@ -0,0 +1,1 @@
+<?php require 'actions/securiteaction.php' ; ?>
