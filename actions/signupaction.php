07044972459c505725ba4dc5e4299a19e91b3fb3
wassimouricha <wbouricha5@gmail.com>
2022-03-23 11:28:31 +0100

    latest update php

---
+++ actions/signupaction.php
@@ -0,0 +1,8 @@
+<!-- MVC : 
+
+M : le model c'est ce qui va se charger de récuperer des données  et de faire des requetes 
+V : View (vue) sera tout ce qui est affiché à l'utilisateur 
+C :   le controlleur , il va controller toute les données , le controlleur va aller appeller le model , récuperer les données puis les afficher 
+
+-->
+
