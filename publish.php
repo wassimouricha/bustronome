7d378d3a4230b51a8115332dbe12c0f31ab91128
wassimouricha <wbouricha5@gmail.com>
2022-03-25 13:41:01 +0100

    calendar added php

--- publish.php
+++ publish.php
@@ -14,42 +14,15 @@
             <!-- sidebar -->
             <?php include 'includes/sidebar.php' ; ?>
            
-            <div class="containere">
-        
-        <?php 
+            <div class="containered">
+                  <div class="calendriercont">
 
-         if(isset($errorMsg)){echo '<p class="messagus">'.$errorMsg.'</p>'; }  ?>
-<br> <br>
-     <form class="container" method="POST"> <!-- ici j'ajoute la method HTTP post afin d'indiquer que je vais envoyer des informations -->
-             <div class="mb-3">
-                 <label for="exampleInputEmail1" class="form-label">Pseudo</label>
-                 <input type="text" class="form-control" name="pseudo">
-                 <div id="emailHelp" class="form-text">Je ne vais jamais partager vos informations vous pensez bien</div>
-             </div>
-             <div class="mb-3">
-                 <label for="exampleInputEmail1" class="form-label">Nom</label>
-                 <input type="text" class="form-control" name="lastname">
-             </div>
-             <div class="mb-3">
-                 <label for="exampleInputEmail1" class="form-label">Prénom</label>
-                 <input type="text" class="form-control" name="firstname">
-             </div>
-
-             <div class="mb-3">
-                 <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
-                 <input type="password" class="form-control" name="password">
-             </div>
-     
-             <button type="submit" class="btn btn-dark" name="validate">S'inscrire</button>
-             <br> <br>
-             <a href="login.php" ><p>déjà un compte ? connectez-vous</p></a>
-     </form>
- 
-
+                  </div>
+       
     
        
 
-</div>
+              </div>
          
 
             
