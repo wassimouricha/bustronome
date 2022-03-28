d0516edcd778bb9ee99931a0515a6b37058e8777
wassimouricha <wbouricha5@gmail.com>
2022-03-28 14:57:28 +0200

    latest update

--- actions/publishquestionaction.php
+++ actions/publishquestionaction.php
@@ -12,20 +12,20 @@ if(isset($_POST['validate'])){
        $question_title = htmlspecialchars($_POST['title']);
        $question_description = nl2br(htmlspecialchars($_POST['description']));   //nl2br permet d'autoriser les sauts de ligne dans le textarea
        $question_content = nl2br(htmlspecialchars($_POST['content']));
-       $question_date = date('d/m/Y');
        $question_id_author = $_SESSION['id'];
        $question_pseudo_author = $_SESSION['pseudo'];
+       $question_date = date('d/m/Y');
 
         $insertQuestionOnWebsite = $bdd->prepare('INSERT INTO questions(titre, description, contenu, id_auteur, pseudo_auteur, date_publication)VALUES(?, ?, ?, ?, ?, ?,)');
         $insertQuestionOnWebsite->execute(
-            array(
+            [
                 $question_title,  
                 $question_description, 
                 $question_content, 
                 $question_id_author, 
                 $question_pseudo_author,  
                 $question_date
-                )
+            ]
             );
 
             $successMsg = "Votre Menu à bien été publié sur le site !";
