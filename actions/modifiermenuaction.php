d2815907535d625c91074069ec345280e5a5e696
wassimouricha <wbouricha5@gmail.com>
2022-03-29 14:38:41 +0200

    latest update

--- actions/modifiermenuaction.php
+++ actions/modifiermenuaction.php
@@ -6,8 +6,8 @@ require('actions/database.php');
 if(isset($_GET['id'])  AND !empty($_GET['id'])){
 
     $idofmenu = $_GET['id'];  //je met le get id dans une variable pour éviter de le réecrire
-    //on va venir récuperer l'id d'un menu qui se trouve dans la base de donnée menu(sus phpmyadmin) qui possede l'id que l'on a mis dans l'url
-    $checkIfMenuExist = $bdd->prepare('SELECT id FROM menu WHERE id = ?');
+    //on va venir récuperer l'id et l'id_auteur d'un menu qui se trouve dans la base de donnée menu(sur phpmyadmin) qui possede l'id que l'on a mis dans l'url
+    $checkIfMenuExist = $bdd->prepare('SELECT id, id_auteur FROM menu WHERE id = ?');
     $checkIfMenuExist->execute(array($idofmenu));
 
     //rowcount nous permet de compter le nombre de donéne récuperer par cette requete
@@ -15,7 +15,15 @@ if(isset($_GET['id'])  AND !empty($_GET['id'])){
     if($checkIfMenuExist->rowCount() > 0){
 
         //ici on va verifier que l'identifiant de l'auteur qui est connecté est bien l'auteur du menu que l'on veut modifier
-        
+        //fetch pour recuperer toutes les données de cette requete
+        $menuInfo = $checkIfMenuExist->fetch();
+         // is l'id auteur est le meme que l'id de l'auteur la session qui est connectée et bien on pourra exectuer le code
+        if($menuInfo['id_auteur'] == $_SESSION['id']){
+
+
+        }else{
+            $errorMsg = "Vous n'etes pas l'auteur de cette question et donc vous n'avez pas le droit de la modifier" ;
+        }
 
     }else{
         $errorMsg = "Aucun menu ne possède l'id en question !" ;
