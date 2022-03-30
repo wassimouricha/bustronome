848af8c9161e9c6bd59e575430355f1f0fe47934
wassimouricha <wbouricha5@gmail.com>
2022-03-30 13:36:08 +0200

    latest update

---
+++ profile.php
@@ -0,0 +1,51 @@
+<?php 
+
+
+   
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- En tete -->
+
+<?php include 'includes/head.php' ; ?>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+<!-- Body -->
+<body class="main">
+
+
+            <!-- sidebar -->
+            <?php include 'includes/sidebar.php' ; ?>
+           
+            <div class="containered">
+                <ul class="listemenud">
+                 <li> <a href="profile.php"> <i class="fa-solid fa-calendar"> <span> Mon Profil </span>  </i></a>  </li>
+               
+                </ul>
+                <div class="titrecalend">
+                  <h1 >
+                    Voici Mon Profil
+                  </h1>
+                  
+                </div>
+                 
+
+              </div>
+         
+
+            
+            <!-- footer -->
+            <?php include 'includes/footer.php' ; ?>
+
+    <!-- Script -->
+      <script src="//code.jquery.com/jquery-1.12.4.js"></script>
+      <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+      <script>
+      $( "#datepicker" ).datepicker();
+      </script>
+      <script src="./script.js"></script>
+</body>
+</html>
