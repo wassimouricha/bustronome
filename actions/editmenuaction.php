da465cc10ce35ef2725c0a71a2cc17fda01fde68
wassimouricha <wbouricha5@gmail.com>
2022-03-31 09:24:02 +0200

    latest update

--- actions/editmenuaction.php
+++ actions/editmenuaction.php
@@ -15,7 +15,7 @@ if(isset($_POST['validate'])){
 
             //update est une requete permettant de mettre à jour des informations dans notre table
             //ici on lui dit de mettre a jour la table menu , il va mettre à jour le titre, le description et le contenu du menu qui possede l'id soit donc un id existant déjà dans la table menu
-        $editmenu = $bdd->prepare('UPDATE menu SET titre = ?, descriptioned = ?, content = ?, image = ? WHERE id = ?');
+        $editmenu = $bdd->prepare('UPDATE menu SET titre = ?, descriptioned = ?, content = ?  WHERE id = ?');
         //on execute la requete qui dans un tableau va venir récuperer le $_GET['id'] (ou ici la variable $idofmenu) soit donc la variable id 
         $editmenu->execute(array($newmenutitle, $newmenudescriptioned, $newmenucontent, $_GET['id']));
 
