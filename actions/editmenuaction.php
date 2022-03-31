b35f33c5d7f0f2614028a96204c93f9a840d6901
wassimouricha <wbouricha5@gmail.com>
2022-03-31 09:21:33 +0200

    latestupdate

--- actions/editmenuaction.php
+++ actions/editmenuaction.php
@@ -11,13 +11,13 @@ if(isset($_POST['validate'])){
         $newmenutitle = htmlspecialchars($_POST['title']);
         $newmenudescriptioned = htmlspecialchars($_POST['descriptioned']);
         $newmenucontent = htmlspecialchars($_POST['content']);
-        $newmenuimage = file_get_contents($_FILES['image']['tmp_name']);
+        //$newmenuimage = file_get_contents($_FILES['image']['tmp_name']);
 
             //update est une requete permettant de mettre à jour des informations dans notre table
             //ici on lui dit de mettre a jour la table menu , il va mettre à jour le titre, le description et le contenu du menu qui possede l'id soit donc un id existant déjà dans la table menu
         $editmenu = $bdd->prepare('UPDATE menu SET titre = ?, descriptioned = ?, content = ?, image = ? WHERE id = ?');
         //on execute la requete qui dans un tableau va venir récuperer le $_GET['id'] (ou ici la variable $idofmenu) soit donc la variable id 
-        $editmenu->execute(array($newmenutitle, $newmenudescriptioned, $newmenucontent, $newmenuimage, $_GET['id']));
+        $editmenu->execute(array($newmenutitle, $newmenudescriptioned, $newmenucontent, $_GET['id']));
 
         header('Location: mes-menu.php');
 
